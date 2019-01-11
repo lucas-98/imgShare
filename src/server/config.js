ca59cb1160493d4273ca4261bd9a1b314251a120
Lucas Carranza <lucas.carranza98@gmail.com>
2019-01-11 00:21:12 -0300

    tranajando en las vistas --carpeta views

--- src/server/config.js
+++ src/server/config.js
@@ -13,8 +13,8 @@ module.exports = app => {
 
     //Settings
     app.set('port', process.env.PORT || 3000);
-    app.set('views', path.join(__dirname, 'views'));
-    app.engine('.hbs', exhbs({
+    app.set('views', path.join(__dirname, '../views'));
+    app.engine('.hbs', exphbs({
         defaultLayout: 'main',
         partialsDir: path.join(app.get('views'), 'partials'),
         layoutsDir: path.join(app.get('views'), 'layouts'),
